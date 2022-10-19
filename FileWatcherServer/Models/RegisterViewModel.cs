5ac528c04a6b1f2908a15570227aa29eb0cc4b99
PBoldi <boldi333333@gmail.com>
2022-10-19 11:49:39 +0200

    username

--- FileWatcherServer/Models/RegisterViewModel.cs
+++ FileWatcherServer/Models/RegisterViewModel.cs
@@ -3,6 +3,9 @@
     public class RegisterViewModel
     {
         public string Email { get; set; }
+        public string UserName { get; set; }
+
         public string Password { get; set; }
+
     }
 }
