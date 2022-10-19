ce1861f81e35b1c1f766a0bea0107ff0744e812d
PBoldi <boldi333333@gmail.com>
2022-10-19 12:30:06 +0200

    ...

--- FileWatcherServer/Controllers/WatcherController.cs
+++ FileWatcherServer/Controllers/WatcherController.cs
@@ -1,5 +1,6 @@
 ﻿using FileWatcherServer.Hubs;
 using FileWatcherServer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 
@@ -8,11 +9,17 @@ namespace FileWatcherServer.Controllers
     public class WatcherController : ControllerBase
     {
         IHubContext<EventHub> hub;
+        UserManager<IdentityUser> _userManager;
 
+        public WatcherController(IHubContext<EventHub> hub, UserManager<IdentityUser> userManager)
+        {
+            this.hub = hub;
+            _userManager = userManager;
+        }
         [HttpPost]
         public async void Changed([FromBody] string message)
         {
-            WatcherModel wm=new WatcherModel();
+            WatcherModel wm = new WatcherModel();
             wm.UserName= this.User.Identity.Name;
             wm.Message = message;
             await hub.Clients.All.SendAsync("Changed", wm);
