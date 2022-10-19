e2f5cbcf1aaf35f8fdf02d2be56fa4d8c0ad2202
PBoldi <boldi333333@gmail.com>
2022-10-19 10:33:54 +0200

    programcs konfig

--- FileWatcherServer/Program.cs
+++ FileWatcherServer/Program.cs
@@ -1,11 +1,57 @@
+using FileWatcherServer.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<ApiDbContext>(option =>
+{
+    option
+    .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=FileWatcherServer;Trusted_Connection=True;MultipleActiveResultSets=true")
+    .UseLazyLoadingProxies();
+});
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(option =>
+{
+    option.Password.RequiredLength = 8;
+    option.Password.RequireNonAlphanumeric = false;
+})
+  .AddEntityFrameworkStores<ApiDbContext>()
+  .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = "http://www.security.org",
+        ValidIssuer = "http://www.security.org",
+        IssuerSigningKey = new SymmetricSecurityKey
+      (Encoding.UTF8.GetBytes("vmihosszukodajwttokenhez"))
+    };
+});
+builder.Services.AddAuthorization();
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
 
 var app = builder.Build();
 
@@ -18,6 +64,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//app.MapHub<EventHub>("/events");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
