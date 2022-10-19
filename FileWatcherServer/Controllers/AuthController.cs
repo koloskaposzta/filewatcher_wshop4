121d8f1e7799bbbccef909ffabbd44ce14a70af7
PBoldi <boldi333333@gmail.com>
2022-10-19 10:40:54 +0200

    login

---
+++ FileWatcherServer/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using FileWatcherServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FileWatcherServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var claim = new List<Claim>
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+                };
+                foreach (var role in await _userManager.GetRolesAsync(user))
+                {
+                    claim.Add(new Claim(ClaimTypes.Role, role));
+                }
+                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("vmihosszukodajwttokenhez"));
+                var token = new JwtSecurityToken(
+                 issuer: "http://www.security.org", audience: "http://www.security.org",
+                 claims: claim, expires: DateTime.Now.AddMinutes(60),
+                 signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+    }
+}
